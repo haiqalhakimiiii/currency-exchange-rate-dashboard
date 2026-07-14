82ecccfebc8f000a6cfa82c208a1d4651e2f7ee3
haiqal <haiqalhakimiiii@gmail.com>
2026-07-14 21:55:19 +0800

    fix the registering of admin layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,10 @@
 import { Routes } from '@angular/router';
-import { DashboardLayoutModule } from './layouts/dashboard-layout/dashboard-layout.module';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
     path: '',
-    component: DashboardLayoutModule,
-    children: [
-      {
-        path: '',
-        loadChildren: () =>
-          import('./layouts/dashboard-layout/dashboard-layout.module').then((m) => m.DashboardLayoutModule),
-      },
-    ],
+    loadChildren: () =>
+      import('./layouts/dashboard-layout/dashboard-layout.module').then((m) => m.DashboardLayoutModule),
   },
 ];
