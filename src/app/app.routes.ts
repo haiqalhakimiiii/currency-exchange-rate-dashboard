e53cbf32a915df87f10ca584e8bb98c8ea12f8b5
haiqal <haiqalhakimiiii@gmail.com>
2026-07-14 21:42:42 +0800

    register dashboard layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { DashboardLayoutModule } from './layouts/dashboard-layout/dashboard-layout.module';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardLayoutModule,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./layouts/dashboard-layout/dashboard-layout.module').then((m) => m.DashboardLayoutModule),
+      },
+    ],
+  },
+];
